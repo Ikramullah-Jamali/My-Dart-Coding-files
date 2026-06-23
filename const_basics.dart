00bf0fcc233909f1eb6aea43dca601bbbf0d63bc
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-06-24 00:06:25 +0500

    Add files via upload

---
+++ const_basics.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  const int age=20;
+  const String name="saqib";
+
+  const List<int> numbers=[1,2,3];
+  const Map<String,int>marks={"math":90,"CS":95,};
+
+  print(age);
+  print(name);
+  print(numbers);
+  print(marks);
+}
